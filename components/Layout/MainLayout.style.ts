3b1c3b8e52a2ddb50acaab63b79f11645afa1002
BonhaengLee <bon952014@ajou.ac.kr>
2022-10-09 23:34:09 +0900

    feat: MainLayout style 분리, logo를 svg로 대체

---
+++ components/Layout/MainLayout.style.ts
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  padding: 0 265px;
+`;
+
+export const Header = styled.header`
+  height: 103px;
+  padding-top: 30px;
+`;
--- components/Layout/MainLayout.tsx
+++ components/Layout/MainLayout.tsx
@@ -1,32 +1,8 @@
 import React from "react";
 import Link from "next/link";
-import styled from "@emotion/styled";
+import Image from "next/image";
 
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  padding: 0 265px;
-`;
-
-const Header = styled.header`
-  height: 103px;
-  padding-top: 30px;
-`;
-// Logo 임시 스타일링
-const FirstLogoText = styled.span`
-  font-weight: 700;
-  font-size: 34px;
-  line-height: 42.57px;
-  margin-right: 8px;
-  color: #26409a;
-`;
-
-const SecondLogoText = styled.span`
-  font-weight: 500;
-  font-size: 34px;
-  line-height: 42.57px;
-  color: #26409a;
-`;
+import { Container, Header } from "./MainLayout.style";
 
 interface Props {
   children: React.ReactNode;
@@ -38,8 +14,12 @@ export default function MainLayout({ children }: Props) {
       <Header>
         <Link href="/">
           <a>
-            <FirstLogoText>AYU</FirstLogoText>
-            <SecondLogoText>campus</SecondLogoText>
+            <Image
+              alt="ayu campus logo"
+              src="/images/ayu-campus-logo.svg"
+              width="212px"
+              height="43px"
+            />
           </a>
         </Link>
       </Header>

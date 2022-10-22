1684ab9886733a1cee861eb2220f1b95386a85f7
BonhaengLee <bon952014@ajou.ac.kr>
2022-10-22 14:06:49 +0900

    feature: twin.macro & emotion 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { GlobalStyles } from "twin.macro";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <GlobalStyles />
+      <Component {...pageProps} />;
+    </div>
+  );
 }
 
 export default MyApp;

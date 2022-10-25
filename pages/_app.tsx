bf8bfccef4203cd46ebfe51ff6ea6add793bc5ed
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-10-25 11:44:58 +0900

    feat: 레이아웃 구현

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
-import "../styles/reset.css";
-import "../styles/globals.css";
+import React from "react";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import "../styles/reset.css";
+import "../styles/globals.css";
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: React.ReactElement) => React.ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || (page => page);
+
+  return <>{getLayout(<Component {...pageProps} />)}</>;
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
+import React from "react";
+
+import MainLayout from "../components/Layout/MainLayout";
+
 function Home() {
-  return <div>홈입니다.</div>;
+  return <div>dd</div>;
 }
+
+Home.getLayout = function getLayout(page: React.ReactElement) {
+  return <MainLayout>{page}</MainLayout>;
+};
 
 export default Home;

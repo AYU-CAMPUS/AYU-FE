65dc9df55cba95ee65d97626a10799d92f705cd0
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-10-28 13:48:21 +0900

    Add: eslint 룰 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,6 +42,7 @@ module.exports = {
     "@typescript-eslint/no-unused-vars": "error",
     "@typescript-eslint/no-explicit-any": "error",
     "react/react-in-jsx-scope": "off",
+    "import/no-unresolved": "off",
     "react/jsx-filename-extension": [
       1,
       {

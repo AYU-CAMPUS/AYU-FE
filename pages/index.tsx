8d968d17542454947cf0491a3e282cb05d626744
Coa <coalee22@gmail.com>
2022-11-07 10:28:00 +0900

    feature: tailwind.config 세팅 (#10)

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,13 @@ function Home() {
     <div
       css={[
         tw`flex flex-col items-center justify-center h-screen`,
-        tw`bg-gradient-to-b from-electric to-ribbon`,
+        tw`bg-gradient-to-b `,
       ]}
     >
       <div tw="flex flex-col justify-center h-full space-y-5">
-        <Button isPrimary>Submit</Button>
+        <Button isPrimary tw="text-h3 text-primary-5">
+          Submit
+        </Button>
         <Button isSecondary>Cancel</Button>
         <Button isSmall>Close</Button>
       </div>

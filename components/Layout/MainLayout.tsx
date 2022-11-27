4cb940384d2d868698d259dcd7eb9dd71ceaf6fd
BonhaengLee <bon952014@ajou.ac.kr>
2022-11-27 19:20:22 +0900

    feat: 헤더

--- components/Layout/MainLayout.tsx
+++ components/Layout/MainLayout.tsx
@@ -1,16 +1,42 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { Container, Header } from "./MainLayout.style";
+import { css } from "@emotion/react";
+import tw from "twin.macro";
 
 interface Props {
   children: React.ReactNode;
+  links?: React.ReactNode[];
+  notifications?: React.ReactNode[];
 }
 
-export default function MainLayout({ children }: Props) {
+export default function MainLayout({ children, links, notifications }: Props) {
   return (
-    <Container>
-      <Header>
+    <div
+      css={[
+        tw``,
+        css`
+          width: 100vw;
+          height: 100vh;
+        `,
+      ]}
+    >
+      <header
+        css={[
+          tw``,
+          css`
+            width: 100vw;
+            height: 103px;
+            padding-top: 30px;
+            padding-bottom: 30px;
+            border-bottom: 1px solid #abaeaf;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+          `,
+        ]}
+      >
+        {/* // 로고 링크 */}
         <Link href="/">
           <a>
             <Image
@@ -21,8 +47,39 @@ export default function MainLayout({ children }: Props) {
             />
           </a>
         </Link>
-      </Header>
-      {children}
-    </Container>
+        {/* 로고를 제외하고 우측 배치 */}
+        <section
+          css={[
+            css`
+              display: flex;
+              align-items: center;
+
+              & > * {
+                margin-left: 50px;
+              }
+
+              span {
+                a {
+                  display: inline-block;
+                  font-weight: 400px;
+                  font-size: 16px;
+                  line-height: 20px;
+                }
+              }
+            `,
+          ]}
+        >
+          {/* 링크들 */}
+          {links?.map(link => {
+            return link;
+          })}
+          {/* 알림 */}
+          {notifications?.map(notification => {
+            return notification;
+          })}
+        </section>
+      </header>
+      <main>{children}</main>
+    </div>
   );
 }
